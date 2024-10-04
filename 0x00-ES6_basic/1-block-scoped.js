9a492708c880e3358af3d06bdcd5d7a60c6f6085
AustinBenSimiyu <rubberducktoo@gmail.com>
2024-10-05 02:21:09 +0300

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
-  const task3 = task;
-  const task4 = task2;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    task = true;
-    task2 = false;
+    const task = true;
+    const task2 = false;
+    return [task2, task];
   }
-  task = task3;
-  task2 = task4;
   return [task, task2];
 }
